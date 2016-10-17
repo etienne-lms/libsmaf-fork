f3d559d6d8808e922b223b5bceb7d35edab9fd71
Etienne Carriere <etienne.carriere@linaro.org>
2016-10-17 16:38:44 +0200

    test_smaf.c: count errors when testing
    test_smaf return a non null value only if all tests are successful.
    Add some verbosity on errors.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- tests/test_smaf.c
+++ tests/test_smaf.c
@@ -27,231 +27,235 @@
 
 #define LENGTH 1024*16
 
-static void test_create_named(char *name)
+static unsigned int test_create_named(char *name)
 {
 	int ret;
 	int fd;
 
 	ret = smaf_create_buffer(LENGTH, SMAF_CLOEXEC | SMAF_RDWR, name, &fd);
-
-	if (ret || (fd == -1)) {
-		printf("%s: smaf_create_buffer() failed %d\n", __func__, ret);
-		return;
+	if (ret || fd < 0) {
+		printf("%s: smaf_create_buffer(\"%s\") failed %d %d\n",
+			__func__, name ? name : "null", ret, fd);
+		return 1;
 	}
-
-	printf("%s: smaf_create_buffer() for allocator %s successed\n", __func__, name);
-
+	printf("%s: \"%s\" succeeded\n", __func__, name ? name : "null", ret);
 	close(fd);
+	return 0;
 }
 
-static void test_create_named_invalid(void)
+static unsigned int test_create_named_invalid(void)
 {
 	int ret;
 	int fd;
 
 	ret = smaf_create_buffer(LENGTH, SMAF_CLOEXEC | SMAF_RDWR, "deadbeef", &fd);
-
-	if (!ret) {
-		printf("%s: smaf_create_buffer() failed %d\n", __func__, ret);
-		return;
+	if (!ret && fd >= 0) {
+		close(fd);
+		printf("%s: invalid smaf_create_buffer() did not fail: %d/%d\n",
+			__func__, ret, fd);
+		return 1;
 	}
-
-	printf("%s: successed\n", __func__);
+	printf("%s: succeeded\n", __func__);
+	return 0;
 }
 
-static void test_iter_over_allocators(void)
+static unsigned int test_iter_over_allocators(void)
 {
 	int i;
 	int count = smaf_allocator_count();
 	char *name;
+	int err = 0;
 
 	if (count <= 0) {
-		printf("%s: smaf_allocator_count() failed %d\n", __func__, count);
-		return;
+		printf("%s: no allocator %d\n", __func__, count);
+		return 1;
 	}
 
-	printf("%s: smaf_allocator_count() found %d allocators\n", __func__, count);
 	for (i = 0; i < count; i++) {
 		name = smaf_get_allocator_name(i);
 		if (!name) {
 			printf("%s: smaf_get_allocator_name() failed %d\n", __func__, i);
-			return;
+			err++;
+			continue;
 		}
-
-		test_create_named(name);
+		err += test_create_named(name);
 		free(name);
 	}
+	return err;
 }
 
-static void test_invalid_allocator_index(void)
+static unsigned int test_invalid_allocator_index(void)
 {
 	int count = smaf_allocator_count();
 	char *name;
 
 	if (count <= 0) {
 		printf("%s: smaf_allocator_count() failed %d\n", __func__, count);
-		return;
+		return 1;
 	}
 
-	printf("%s: smaf_allocator_count() found %d allocators\n", __func__, count);
 	name = smaf_get_allocator_name(count);
 	if (name) {
-		printf("%s: smaf_get_allocator_name() failed %s\n", __func__, name);
+		printf("%s: smaf_get_allocator_name(%s) did not fail\n", __func__, name);
 		free(name);
-		return;
+		return 1;
 	}
 
-	printf("%s: successed\n", __func__);
+	printf("%s: succeeded\n", __func__);
+	return 0;
 }
 
 
-static void test_secure(void)
+static unsigned int test_secure(void)
 {
 	int ret;
 	int fd;
 
 	ret = smaf_create_buffer(LENGTH, SMAF_CLOEXEC | SMAF_RDWR, NULL, &fd);
-
 	if (ret || (fd == -1)) {
-		printf("%s: smaf_create_buffer() failed %d\n", __func__, ret);
-		return;
+		printf("%s: smaf_create_buffer() failed %d/%d\n", __func__, ret, fd);
+		return 1;
 	}
 
 	ret = smaf_set_secure(fd, 1);
 	if (ret) {
 		printf("%s: smaf_set_secure() failed %d\n", __func__, ret);
+		ret = 1;
 		goto end;
 	}
 
 	ret = smaf_get_secure(fd);
-	if (!ret) {
+	if (!ret) {  // TODO: check this
 		printf("%s: smaf_get_secure() failed %d\n", __func__, ret);
+		ret = 1;
 		goto end;
 	}
 
-	printf("%s: successed\n", __func__);
+	printf("%s: succeeded\n", __func__);
+	ret = 0;
 end:
 	close(fd);
+	return ret;
 }
 
-static void test_create_non_page_aligned_mmap(void)
+static unsigned int test_create_non_page_aligned_mmap(void)
 {
 	int ret;
 	int fd;
 	void *data;
 
 	ret = smaf_create_buffer(LENGTH+1, SMAF_CLOEXEC | SMAF_RDWR, NULL, &fd);
-
-	if (ret || (fd == -1)) {
-		printf("%s smaf_create_buffer() failed %d\n", __func__, ret);
-		return;
+	if (ret || fd < 0) {
+		printf("%s: smaf_create_buffer() failed %d/%d\n", __func__, ret, fd);
+		return 1;
 	}
 
 	data = mmap(0, LENGTH+1, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-
 	munmap(data, LENGTH+1);
 	close(fd);
 
-	if (data == MAP_FAILED)
+	if (data == MAP_FAILED) {
 		printf("%s failed\n", __func__);
-	else
-		printf("%s successed\n", __func__);
+		return 1;
+	}
+	printf("%s succeeded\n", __func__);
+	return 0;
 }
 
-static void test_create_non_page_aligned(void)
+static unsigned int test_create_non_page_aligned(void)
 {
 	int ret;
 	int fd;
 
 	ret = smaf_create_buffer(LENGTH+1, SMAF_CLOEXEC | SMAF_RDWR, NULL, &fd);
-
-	if (ret || (fd == -1)) {
-		printf("%s smaf_create_buffer() failed %d\n", __func__, ret);
-		return;
+	if (ret || fd < 0) {
+		printf("%s: smaf_create_buffer() failed %d/%d\n", __func__, ret, fd);
+		return 1;
 	}
 
 	close(fd);
-	printf("%s successed\n", __func__);
+	printf("%s succeeded\n", __func__);
+	return 0;
 }
 
-static void test_create_unnamed(void)
+static unsigned int test_create_unnamed(void)
 {
-	test_create_named(NULL);
+	return test_create_named(NULL);
 }
 
-static void test_mmap(void)
+static unsigned int test_mmap(void)
 {
-	int ret, fd;
+	int ret;
+	int fd;
 	char *data;
 
 	ret = smaf_create_buffer(LENGTH, SMAF_CLOEXEC | SMAF_RDWR, NULL, &fd);
-
-	if (ret || (fd == -1)) {
-		printf("%s: smaf_create_buffer() failed %d\n", __func__, ret);
-		return;
+	if (ret || fd < 0) {
+		printf("%s: smaf_create_buffer() failed %d/%d\n", __func__, ret, fd);
+		return 1;
 	}
 
 	data = mmap(NULL, LENGTH, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	munmap(data, LENGTH);
+	close(fd);
+
 	if (data == MAP_FAILED) {
 		printf("%s: mmap failed\n", __func__);
-		goto end;
+		return 1;
 	}
-
-	munmap(data, LENGTH);
-	printf("%s: successed\n", __func__);
-end:
-	close(fd);
+	printf("%s: succeeded\n", __func__);
+	return 0;
 }
 
-static void test_mmap_secure(void)
+static unsigned int test_mmap_secure(void)
 {
-	int ret, fd;
+	int ret;
+	int fd;
 	char *data;
 
 	ret = smaf_create_buffer(LENGTH, SMAF_CLOEXEC | SMAF_RDWR, NULL, &fd);
-
-	if (ret || (fd == -1)) {
-		printf("%s: smaf_create_buffer() failed %d\n", __func__, ret);
-		return;
+	if (ret || fd < 0) {
+		printf("%s: smaf_create_buffer() failed %d/%d\n", __func__, ret, fd);
+		return 1;
 	}
 
 	ret = smaf_set_secure(fd, 1);
 	if (ret) {
 		printf("%s: smaf_set_secure() failed %d\n", __func__, ret);
-		goto end;
+		return 1;
 	}
 
 	data = mmap(NULL, LENGTH, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-	if (data == MAP_FAILED) {
-		printf("%s: mmap failed\n", __func__);
-		goto end;
-	}
-
 	munmap(data, LENGTH);
-	printf("%s: successed\n", __func__);
-end:
 	close(fd);
+	if (data != MAP_FAILED) {
+		printf("%s: mmap on secure buffer did not fail\n", __func__);
+		return 1;
+	}
+	printf("%s: succeeded\n", __func__);
+	return 0;
 }
 
 int main (void)
 {
 	if (smaf_open()) {
 		printf("Can't open /dev/smaf\n");
-		return 0;
+		return -1;
 	}
 
-	test_create_unnamed();
+	err += test_create_unnamed();
 
-	test_iter_over_allocators();
-	test_invalid_allocator_index();
-	test_create_named_invalid();
-	test_create_non_page_aligned();
-	test_create_non_page_aligned_mmap();
-	test_secure();
+	err += test_iter_over_allocators();
+	err += test_invalid_allocator_index();
+	err += test_create_named_invalid();
+	err += test_create_non_page_aligned();
+	err += test_create_non_page_aligned_mmap();
+	err += test_secure();
 
-	test_mmap();
-	test_mmap_secure();
+	err += test_mmap();
+	err += test_mmap_secure();
 
 	smaf_close();
-	return 0;
+	printf("smaf test status: nb errors = %d\n", err);
+	return !err ? 0 : 1;
 }
